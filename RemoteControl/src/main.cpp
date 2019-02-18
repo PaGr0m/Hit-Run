e9d36201cb5710181e2da231a148ee2a1d384839
Pavel Gromov <pagrom27@gmail.com>
2019-02-18 22:27:27 +1000

    Separate Sender and Receiver

--- RemoteControl/src/main.cpp
+++ RemoteControl/src/main.cpp
@@ -1,33 +1,33 @@
-#include <Arduino.h>
+/*
+   March 2014 - TMRh20 - Updated along with High Speed RF24 Library fork
+   Parts derived from examples by J. Coliz <maniacbug@ymail.com>
+*/
+/**
+ * Example for efficient call-response using ack-payloads
+ *
+ * This example continues to make use of all the normal functionality of the radios including
+ * the auto-ack and auto-retry features, but allows ack-payloads to be written optionlly as well.
+ * This allows very fast call-response communication, with the responding radio never having to
+ * switch out of Primary Receiver mode to send back a payload, but having the option to switch to
+ * primary transmitter if wanting to initiate communication instead of respond to a commmunication.
+ */
+
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
 #include "printf.h"
 
-// Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 9 & 10
 RF24 radio(9,10);
 
-                                                                           // Topology
-byte addresses[][6] = {"1Node","2Node"};              // Radio pipe addresses for the 2 nodes to communicate.
+byte counter = 1;
 
-// Role management: Set up role.  This sketch uses the same software for all the nodes
-// in this system.  Doing so greatly simplifies testing.
-typedef enum { role_ping_out = 1, role_pong_back } role_e;                 // The various roles supported by this sketch
-const char* role_friendly_name[] = { "invalid", "Ping out", "Pong back"};  // The debug-friendly names of those roles
-role_e role = role_pong_back;                                              // The role of the current running sketch
-
-byte counter = 1;                                                          // A single byte to keep track of the data being sent back and forth
-
-
-void setup(){
-  pinMode(4, OUTPUT);
-  pinMode(2, OUTPUT);
-
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+void setup()
+{
 
   Serial.begin(9600);
   printf_begin();
   printf("\n\rRF24/examples/GettingStarted/\n\r");
-  printf("ROLE: %s\n\r",role_friendly_name[role]);
   printf("*** PRESS 'T' to begin transmitting to the other node\n\r");
 
   // Setup and configure radio
@@ -37,59 +37,28 @@ void setup(){
   radio.enableAckPayload();               // Allow optional ack payloads
   radio.setRetries(0,15);                 // Smallest time between retries, max no. of retries
   radio.setPayloadSize(1);                // Here we are sending 1-byte payloads to test the call-response speed
-  radio.openWritingPipe(addresses[1]);        // Both radios listen on the same pipes by default, and switch when writing
-  radio.openReadingPipe(1,addresses[0]);      // Open a reading pipe on address 0, pipe 1
+
+  radio.openReadingPipe(1, pipe);      // Open a reading pipe on address 0, pipe 1
   radio.startListening();                 // Start listening
   radio.powerUp();
   radio.printDetails();                   // Dump the configuration of the rf unit for debugging
 }
 
-void loop(void) {
-
-
-
+void loop(void)
+{
 /****************** Pong Back Role ***************************/
+/********************* RECEIVER ******************************/
 
-  if ( role == role_pong_back ) {
+
     byte pipeNo, gotByte;                          // Declare variables for the pipe and the byte received
-    while( radio.available(&pipeNo)){              // Read all available payloads
+    while( radio.available(&pipeNo))
+    {              // Read all available payloads
       radio.read( &gotByte, 1 );
                                                    // Since this is a call-response. Respond directly with an ack payload.
                                                    // Ack payloads are much more efficient than switching to transmit mode to respond to a call
-      radio.writeAckPayload(pipeNo,&gotByte, 1 );  // This can be commented out to send empty payloads.
+      radio.writeAckPayload(pipeNo, &gotByte, 1 );  // This can be commented out to send empty payloads.
       printf("Sent response %d \n\r", gotByte);
-
-      tone(2, 3500, 2000);
-      digitalWrite(4, HIGH);
-      delay(2000);
-   }
  }
 
- digitalWrite(4, LOW);
 
-
-
-/****************** Change Roles via Serial Commands ***************************/
-
-  if ( Serial.available() )
-  {
-    char c = toupper(Serial.read());
-    if ( c == 'T' && role == role_pong_back )
-    {
-      printf("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK\n\r");
-
-      role = role_ping_out;                      // Change roles (ping out)
-      radio.openWritingPipe(addresses[0]);       // Open different pipes when writing. Write on pipe 0, address 0
-      radio.openReadingPipe(1,addresses[1]);     // Read on pipe 1, as address 1
-    }
-    else if ( c == 'R' && role == role_ping_out )
-    {
-      printf("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK\n\r");
-
-       role = role_pong_back;                    // Become the primary receiver (pong back)
-       radio.openWritingPipe(addresses[1]);      // Since only two radios involved, both listen on the same addresses and pipe numbers in RX mode
-       radio.openReadingPipe(1,addresses[0]);    // then switch pipes & addresses to transmit.
-       radio.startListening();                   // Need to start listening after opening new reading pipes
-    }
-  }
 }
--- WeaponSender/src/main.cpp
+++ WeaponSender/src/main.cpp
@@ -1,32 +1,33 @@
-#include <Arduino.h>
+/*
+   March 2014 - TMRh20 - Updated along with High Speed RF24 Library fork
+   Parts derived from examples by J. Coliz <maniacbug@ymail.com>
+*/
+/**
+ * Example for efficient call-response using ack-payloads
+ *
+ * This example continues to make use of all the normal functionality of the radios including
+ * the auto-ack and auto-retry features, but allows ack-payloads to be written optionlly as well.
+ * This allows very fast call-response communication, with the responding radio never having to
+ * switch out of Primary Receiver mode to send back a payload, but having the option to switch to
+ * primary transmitter if wanting to initiate communication instead of respond to a commmunication.
+ */
+
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
 #include "printf.h"
 
-#define pinButton 4
-
 // Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 9 & 10
 RF24 radio(9,10);
-                                                                           // Topology
-byte addresses[][6] = {"1Node","2Node"};              // Radio pipe addresses for the 2 nodes to communicate.
-
-// Role management: Set up role.  This sketch uses the same software for all the nodes
-// in this system.  Doing so greatly simplifies testing.
-typedef enum { role_ping_out = 1, role_pong_back } role_e;                 // The various roles supported by this sketch
-const char* role_friendly_name[] = { "invalid", "Ping out", "Pong back"};  // The debug-friendly names of those roles
-role_e role = role_pong_back;                                              // The role of the current running sketch
 
 byte counter = 1;                                                          // A single byte to keep track of the data being sent back and forth
-byte cnt = 5;
 
-void setup() {
-  pinMode(pinButton, INPUT_PULLUP);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+void setup(){
 
   Serial.begin(9600);
   printf_begin();
   printf("\n\rRF24/examples/GettingStarted/\n\r");
-  printf("ROLE: %s\n\r", role_friendly_name[role]);
   printf("*** PRESS 'T' to begin transmitting to the other node\n\r");
 
   // Setup and configure radio
@@ -36,8 +37,8 @@ void setup() {
   radio.enableAckPayload();               // Allow optional ack payloads
   radio.setRetries(0,15);                 // Smallest time between retries, max no. of retries
   radio.setPayloadSize(1);                // Here we are sending 1-byte payloads to test the call-response speed
-  radio.openWritingPipe(addresses[1]);        // Both radios listen on the same pipes by default, and switch when writing
-  radio.openReadingPipe(1,addresses[0]);      // Open a reading pipe on address 0, pipe 1
+  radio.openWritingPipe(pipe);        // Both radios listen on the same pipes by default, and switch when writing
+
   radio.startListening();                 // Start listening
   radio.powerUp();
   radio.printDetails();                   // Dump the configuration of the rf unit for debugging
@@ -45,65 +46,28 @@ void setup() {
 
 void loop(void) {
 
-/***************/
-  boolean button = !digitalRead(pinButton);
 
-    if (role == role_ping_out)
-    {
-        if (button)
-        {
-          byte gotByte;                                           // Initialize a variable for the incoming response
+/****************** Ping Out Role ***************************/
+/********************** SENDER *******************************/
 
-          radio.stopListening();                                  // First, stop listening so we can talk.
-          printf("Now sending %d as payload. ",counter);          // Use a simple byte counter as payload
-          unsigned long time = micros();
+    byte gotByte;                                           // Initialize a variable for the incoming response
 
-          if (radio.write(&cnt, 1))
-          {
-              if(!radio.available())
-              {
-                  printf("Got blank response. round-trip delay: %lu microseconds\n\r",micros()-time);
-              }
-              else
-              {
-                  while(radio.available())
-                  {
-                      radio.read( &gotByte, 1 );
-                      printf("Got response %d, round-trip delay: %lu microseconds\n\r",gotByte,micros()-time);
-                      cnt++;
-                  }
-              }
-        }
-        else
-        {
-          printf("Sending failed.\n\r");
+    radio.stopListening();                                  // First, stop listening so we can talk.
+    printf("Now sending %d as payload. ",counter);          // Use a simple byte counter as payload
+    unsigned long time = micros();                          // Record the current microsecond count
+
+    if ( radio.write(&counter, 1) ){                         // Send the counter variable to the other radio
+        if(!radio.available()){                             // If nothing in the buffer, we got an ack but it is blank
+            printf("Got blank response. round-trip delay: %lu microseconds\n\r",micros()-time);
+        }else{
+            while(radio.available() ){                      // If an ack with payload was received
+                radio.read( &gotByte, 1 );                  // Read it, and display the response time
+                printf("Got response %d, round-trip delay: %lu microseconds\n\r",gotByte,micros()-time);
+                counter++;                                  // Increment the counter variable
+            }
         }
 
-        delay(1000);
-      }
+    }else{        printf("Sending failed.\n\r"); }          // If no ack response, sending failed
+
+    delay(1000);  // Try again later
   }
-
-/****************** Change Roles via Serial Commands ***************************/
-
-  if ( Serial.available() )
-  {
-    char c = toupper(Serial.read());
-    if ( c == 'T' && role == role_pong_back )
-    {
-      printf("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK\n\r");
-
-      role = role_ping_out;                      // Change roles (ping out)
-      radio.openWritingPipe(addresses[0]);       // Open different pipes when writing. Write on pipe 0, address 0
-      radio.openReadingPipe(1,addresses[1]);     // Read on pipe 1, as address 1
-    }
-    else if ( c == 'R' && role == role_ping_out )
-    {
-      printf("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK\n\r");
-
-       role = role_pong_back;                    // Become the primary receiver (pong back)
-       radio.openWritingPipe(addresses[1]);      // Since only two radios involved, both listen on the same addresses and pipe numbers in RX mode
-       radio.openReadingPipe(1,addresses[0]);    // then switch pipes & addresses to transmit.
-       radio.startListening();                   // Need to start listening after opening new reading pipes
-    }
-  }
-}
